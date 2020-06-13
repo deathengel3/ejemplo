30286c9e0ae93aba3c2fb1f034fa94abae51f9ff
Noel Alejandro <alejandro-3-6@hotmail.com>
2020-06-12 23:58:52 -0500

    Primer commit

---
+++ src/Formulario.js
@@ -0,0 +1,35 @@
+import React from "react";
+
+class Formulario extends React.Component {
+    state = {
+      values: []
+    };
+    componentDidMount() {
+        this.datos();
+    }
+
+    datos = async () => {
+        await fetch('https://localhost:5001/weatherforecast')
+            .then(datos => datos.json())
+            .then(datos => {
+                console.log(datos);
+                this.setState({values: datos});
+            })
+            .catch(err => console.log(err));
+    }
+    render()
+    {
+        return (
+            <div>
+                {
+                    this.state.values.map((item, index) => (
+                        <h1 key={index}>{item.date}</h1>
+                    ))
+                }
+                <h1>Hello world!</h1>
+            </div>
+        );
+    }
+}
+
+export default Formulario;
